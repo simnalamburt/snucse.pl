5006b47313e776275d8207a5509a497510f59b65
Hyeon Kim <simnalamburt@gmail.com>
2014-09-14 06:28:23 +0900

    Wrote skeletal codes

--- treasure.ml
+++ treasure.ml
@@ -1,1 +1,22 @@
-print_endline "Hello, world!"
+type treasure = StarBox | NameBox of string
+type key = Bar | Node of key * key
+type map =
+  | End of treasure
+  | Branch of map * map
+  | Guide of string * map
+
+(* TODO : implement *)
+let getReady map = [Bar; Bar; Bar]
+
+
+(* test codes below *)
+let explore map =
+  let rec string_of_key key =
+    match key with
+    | Bar -> "-"
+    | Node(k1,k2) -> "(" ^ string_of_key k1 ^ "," ^ string_of_key k2 ^ ")"
+  in
+  print_endline (
+    "{" ^
+    String.concat ", " (List.map string_of_key (getReady map)) ^
+    "}")
