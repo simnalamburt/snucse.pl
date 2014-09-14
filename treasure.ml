4caa538293de8839ef7fd8b31afd1d1e73362495
Hyeon Kim <simnalamburt@gmail.com>
2014-09-14 11:11:08 +0900

    Add a simple TDD platform

--- treasure.ml
+++ treasure.ml
@@ -6,17 +6,4 @@ type map =
   | Guide of string * map
 
 (* TODO : implement *)
-let getReady map = [Bar; Bar; Bar]
-
-
-(* test codes below *)
-let explore map =
-  let rec string_of_key key =
-    match key with
-    | Bar -> "-"
-    | Node(k1,k2) -> "(" ^ string_of_key k1 ^ "," ^ string_of_key k2 ^ ")"
-  in
-  print_endline (
-    "{" ^
-    String.concat ", " (List.map string_of_key (getReady map)) ^
-    "}")
+let getReady map = [Bar]
