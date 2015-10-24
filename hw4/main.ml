f374dece9bcfa6347754937dd457007ad891cb75
Hyeon Kim <simnalamburt@gmail.com>
2015-10-25 00:09:51 +0900

    Remove unnecessary files

--- hw4/main.ml
+++ hw4/main.ml
@@ -1,1 +1,9 @@
-src/main.ml
+type treasure = StarBox | NameBox of string
+type key = Bar | Node of key * key
+type map =
+  | End of treasure
+  | Branch of map * map
+  | Guide of string * map
+
+(* TODO : implement *)
+let getReady map = [Bar]
