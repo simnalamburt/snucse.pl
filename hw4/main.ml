1d5aa80dd5576902200c92732563b1b2586a5196
Hyeon Kim <simnalamburt@gmail.com>
2015-10-25 03:33:30 +0900

    시작

--- hw4/main.ml
+++ hw4/main.ml
@@ -1,9 +1,30 @@
 type treasure = StarBox | NameBox of string
 type key = Bar | Node of key * key
-type map =
-  | End of treasure
-  | Branch of map * map
-  | Guide of string * map
+type map = End of treasure
+         | Branch of map * map
+         | Guide of string * map
 
-(* TODO : implement *)
-let getReady map = [Bar]
+exception IMPOSSIBLE
+
+let getReady (expression: map): key list =
+  let rec inference (expression: map): key =
+    match expression with
+    | End(value) -> begin
+      (* Term *)
+      match value with
+      | StarBox -> (* Constant *) Bar
+      | NameBox(name) -> begin
+        (* Variable, TODO *)
+        Bar
+      end
+    end
+    | Branch(efunc, eparam) -> begin
+      (* Function call, TODO *)
+      Bar
+    end
+    | Guide(name, body) -> begin
+      (* Function definition, TODO *)
+      Bar
+    end
+  in
+  [inference expression]
