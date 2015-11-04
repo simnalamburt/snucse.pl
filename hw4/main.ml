47d60fbc3982e142d17cc2f02215b73aeffada82
Hyeon Kim <simnalamburt@gmail.com>
2015-11-05 02:55:44 +0900

    모범 답안

--- hw4/main.ml
+++ hw4/main.ml
@@ -1,225 +1,116 @@
 (*
- * Spec
+ * SNU 4190.310 Programming Languages 2015 Fall
+ * Treasure hunt
+ * Jaeseung Choi (jschoi@ropas.snu.ac.kr)
  *)
-(* Term *)
-type treasure = StarBox (* constant *)
-              | NameBox of string (* variable *)
 
-(* Type *)
-type key = Bar (* arbitrary type *)
-         | Node of key * key (* function *)
-
-(* Expression *)
-type map = End of treasure (* term *)
-         | Branch of map * map (* function call *)
-         | Guide of string * map (* function definition *)
+type treasure = StarBox | NameBox of string
+type key = Bar | Node of key * key
+type map = 
+  | End of treasure
+  | Branch of map * map
+  | Guide of string * map
 
 exception IMPOSSIBLE
 
+type var = int
 
-(*
- * Implementation
- *)
-type id = string
+let var_id = ref 0 
 
-type term = Number
-          | Variable of id
+let new_var () =
+  let _ = var_id := !var_id + 1 in
+  !var_id
 
-type expression = Term of term
-                | FnCall of expression * expression
-                | FnDef of id * expression
+type key' = BAR | VAR of var | NODE of (key' * key')
 
-type tyvar = id
-type ty = TyVar of tyvar
-        | T
-        | Function of ty * ty
-type tyenv = (id * ty) list
+type equation = EQ of (key' * key') | AND of (equation * equation)
 
-type substitution = (tyvar * ty) list
+type link = (treasure * key') list
 
-let rec apply (subst: substitution) (ty: ty): ty =
-  match ty with
-  | TyVar right when List.mem_assoc right subst -> List.assoc right subst
-  | Function(tparam, tret) -> Function(apply subst tparam, apply subst tret)
-  | _ -> ty
+type substitution = (var * key') list
 
-let apply_env (subst: substitution) (tyenv: tyenv): tyenv =
-  let apply_entity ((name, ty): (id * ty)): (id * ty) =
-    (name, apply subst ty)
-  in
-  List.map apply_entity tyenv
-
-
-let rec occurs (var: tyvar) (ty: ty): bool =
-  match ty with
-  | TyVar right -> var = right
-  | T -> false
-  | Function(tparam, tret) -> occurs var tparam || occurs var tret
-
-let rec unify (left: ty) (right: ty): substitution =
-  match left, right with
-  | left, right when left = right -> []
-  | TyVar alpha, ty
-  | ty, TyVar alpha when not (occurs alpha ty) -> [(alpha, ty)]
-  | Function(tleft1, tleft2), Function(tright1, tright2) -> begin
-    let subst1 = unify tleft1 tright1 in
-    let subst2 = unify (apply subst1 tleft2) (apply subst1 tright2) in
-    subst2 @ subst1 (* Note: tyvar 중복체크 안해도 됨 *)
-  end
-  | _ -> raise IMPOSSIBLE
-
-
-(* TODO: Remove debug codes *)
-let rec print_ty (ty: ty) =
-  match ty with
-  | TyVar name -> print_string name
-  | T -> print_string "T"
-  | Function(tparam, tret) -> begin
-    print_ty tparam;
-    print_string " -> ";
-    print_ty tret;
-  end
-
-(* TODO: Remove debug codes *)
-let print_tyenv (tyenv: tyenv) =
-  print_string "TyEnv: ";
-  if List.length tyenv = 0 then
-    print_endline "[]"
-  else begin
-    print_string "[";
-    List.iter (fun (name, ty) -> begin
-      print_string name;
-      print_string ": ";
-      print_ty ty;
-      print_string ", ";
-    end) tyenv;
-    print_endline "\b\b]"
-  end
-
-(* TODO: Remove debug codes *)
-let print_subst (subst: substitution) =
-  print_string "Subst: ";
-  if List.length subst = 0 then
-    print_endline "[]"
-  else begin
-    print_string "[";
-    List.iter (fun (name, ty) -> begin
-      print_string name;
-      print_string ": ";
-      print_ty ty;
-      print_string ", ";
-    end) subst;
-    print_endline "\b\b]"
-  end
-
-(* TODO: Remove debug codes *)
-let print_names names =
-  print_string "Names: ";
-  if List.length names = 0 then
-    print_endline "[]"
-  else begin
-    print_string "[";
-    List.iter (Printf.printf "%s, ") names;
-    print_endline "\b\b]"
-  end
-
-
-(*
- * Interface
- *)
-let rec map_to_exp (map: map): expression =
-  match map with
-  | End StarBox -> Term Number
-  | End NameBox name -> Term(Variable name)
-  | Branch(mleft, mright) -> FnCall(map_to_exp mleft, map_to_exp mright)
-  | Guide(name, minner) -> FnDef(name, map_to_exp minner)
-
-let getReady (map: map): key list =
-  (* Stateful functions *)
-  let tyenv: tyenv ref = ref [] in
-  let names = ref [] in
-  let new_variable: unit -> ty =
-    let counter = ref 0 in
-    fun () -> begin
-      let str = Printf.sprintf "α%d" !counter in
-      counter := !counter + 1;
-      TyVar str
-    end
-  in
-  (* Inference type of given expression *)
-  let inference (exp: expression): substitution =
-    (*
-     * The *M* Algorithm
-     *
-     * LEE, Oukseh; YI, Kwangkeun. Proofs about a folklore let-polymorphic type
-     * inference algorithm. ACM Transactions on Programming Languages and Systems
-     * (TOPLAS), 1998, 20.4: 707-723.
-     *)
-    let rec m_algorithm (exp: expression) (ty: ty): substitution =
-      match exp with
-      | Term Number -> unify T ty
-      | Term Variable x -> begin
-        if List.mem_assoc x !tyenv then
-          let tright = List.assoc x !tyenv in
-          unify ty tright
-        else begin
-          (* Note: 교재에 없는 부분 *)
-          let alpha = new_variable () in
-          tyenv := !tyenv @ [(x, alpha)];
-          unify ty alpha
-        end
-      end
-      | FnDef(name, edef) -> begin
-        let alpha1 = new_variable () in
-        let alpha2 = new_variable () in
-        let subst1 = unify (Function(alpha1, alpha2)) ty in
-
-        (* Note: 교재에 없는 부분 *)
-        let _SGamma = apply_env subst1 !tyenv in
-        let _Salpha1 = apply subst1 alpha1 in
-        let hack = begin
-          if not (List.mem_assoc name _SGamma) then []
-          else unify _Salpha1 (List.assoc name _SGamma)
-        end in
-        let subst1 = subst1 @ hack in
-        tyenv := _SGamma @ [(name, _Salpha1)];
-        let subst2 = m_wrapped edef (apply subst1 alpha2) in
-        subst2 @ subst1 (* Note: tyvar 중복체크 안해도 됨 *)
-      end
-      | FnCall(efunc, eparam) -> begin
-        let alpha = new_variable () in
-        let subst1 = m_wrapped efunc (Function(alpha, ty)) in
-        tyenv := apply_env subst1 !tyenv;
-        let subst2 = m_wrapped eparam (apply subst1 alpha) in
-        subst2 @ subst1 (* Note: tyvar 중복체크 안해도 됨 *)
-      end
-    and m_wrapped (exp: expression) (ty: ty): substitution =
-      match exp with
-      | Term term -> begin
-        (* Note: (List.length result) is always 1 *)
-        let result = m_algorithm exp ty in
-        let ids, _ = List.split result in
-        names := !names @ ids;
-        result
-      end
-      | _ -> m_algorithm exp ty
+let rec build_equation : link -> map -> key' -> equation * link = fun lnk m k ->
+  match m with
+  | End StarBox -> (EQ (k, BAR), (StarBox, BAR) :: lnk)
+  | End (NameBox x) ->
+    let (k', lnk') = 
+      try (List.assoc (NameBox x) lnk, lnk) with 
+      Not_found -> let k' = VAR (new_var ()) in (k', (NameBox x, k') :: lnk)
     in
-    m_wrapped exp (new_variable ())
-  in
-  let exp = map_to_exp map in
-  let subst = inference exp in
-  let names = List.map (fun id -> TyVar id) !names in
-  let rec ty_to_key (ty: ty): key =
-    match ty with
-    | T -> Bar
-    | Function(tleft, tright) -> Node(ty_to_key tleft, ty_to_key tright)
-    | TyVar name -> begin
-      if List.mem_assoc name subst then
-        ty_to_key (List.assoc name subst)
-      else
-        (* Note: Undefined variable은 Bar로 간주 *)
-        Bar
-    end
-  in
-  let keys = List.map ty_to_key names in
-  List.sort_uniq compare keys
+    (EQ (k, k'), lnk')
+  | Guide (x, m1) ->
+    let alpha, lnk' = 
+      try (List.assoc (NameBox x) lnk, lnk) with 
+      Not_found -> let k' = VAR (new_var ()) in (k', (NameBox x, k') :: lnk)
+    in
+    let beta = VAR (new_var ()) in
+    let (eqn, lnk'') = build_equation lnk' m1 beta in
+    (AND (eqn, EQ (k, NODE (alpha, beta))), lnk'')
+  | Branch (m1, m2) ->
+    let alpha = VAR (new_var ()) in
+    let (eqn1, lnk') = build_equation lnk m1 (NODE (alpha, k)) in
+    let (eqn2, lnk'') = build_equation lnk' m2 alpha in
+    (AND (eqn1, eqn2), lnk'')
+
+let rec substitute_key : key' -> substitution -> key' = fun k s ->
+  match k with
+  | BAR -> BAR
+  | VAR v -> (try List.assoc v s with Not_found -> VAR v)
+  | NODE (k1, k2) -> NODE (substitute_key k1 s, substitute_key k2 s)
+
+let rec substitute_eqn : equation -> substitution -> equation = fun eqn s ->
+  match eqn with
+  | EQ (k1, k2) -> EQ (substitute_key k1 s, substitute_key k2 s)
+  | AND (eqn1, eqn2) -> AND (substitute_eqn eqn1 s, substitute_eqn eqn2 s)
+  
+let rec does_contain k x = 
+  match k with
+  | BAR -> false
+  | VAR x' -> x = x'
+  | NODE (k1, k2) -> does_contain k1 x || does_contain k2 x
+
+let rec compose_substitution s1 s2 =
+  match s1 with
+  | [] -> s2
+  | (v, k) :: tail_s -> 
+    let s2' = List.map (fun (v', k') -> (v', substitute_key k' [(v, k)])) s2 in
+    compose_substitution tail_s ((v, k) :: s2')
+
+let rec solve_equation eqn cur_subs =
+  match eqn with 
+  | EQ (BAR, BAR) -> cur_subs
+  | EQ (VAR alpha, VAR beta) -> 
+    if alpha = beta then cur_subs else 
+      compose_substitution [alpha, VAR beta] cur_subs
+  | EQ (VAR alpha, k) | EQ (k , VAR alpha) -> 
+    if does_contain k alpha then raise IMPOSSIBLE else
+      compose_substitution [(alpha, k)] cur_subs
+  | EQ (NODE (k1, k2), NODE (k1', k2')) ->
+    let subs = solve_equation (EQ (k1, k1')) [] in
+    let k2 = substitute_key k2 subs in
+    let k2' = substitute_key k2' subs in
+    let subs' = solve_equation (EQ (k2, k2')) [] in
+    compose_substitution (compose_substitution subs' subs) cur_subs
+  | AND (eqn1, eqn2) -> 
+    let new_subs = solve_equation eqn1 cur_subs in
+    solve_equation (substitute_eqn eqn2 new_subs) new_subs
+  | EQ (k1, k2) -> raise IMPOSSIBLE
+
+let rec assign_key k s = 
+  match k with
+  | BAR -> Bar
+  | VAR v -> (try assign_key (List.assoc v s) s with Not_found -> Bar)
+  | NODE (k1, k2) -> Node (assign_key k1 s, assign_key k2 s)
+
+let rec convert_to_key lnk subs = 
+  match lnk with
+  | [] -> []
+  | (tr, k) :: tail_lnk -> 
+    let cur_key = assign_key k subs in
+    let tail_keys = convert_to_key tail_lnk subs in
+    if List.mem cur_key tail_keys then tail_keys else cur_key :: tail_keys
+
+let getReady : map -> key list = fun m ->
+  let (eqn, lnk) = build_equation [] m (VAR (new_var ())) in
+  let subs = solve_equation eqn [] in
+  convert_to_key lnk subs
