7e5348a9d8bb7c3cbf7de86ccef7001e7b288f0e
Hyeon Kim <simnalamburt@gmail.com>
2015-11-10 18:29:13 +0900

    SM5 숙제 시작

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -7,11 +7,20 @@
 open K
 open Sm5
 module Translator = struct
-
-  (* TODO : complete this function  *)
   let rec trans : K.program -> Sm5.command = function
     | K.NUM i -> [Sm5.PUSH (Sm5.Val (Sm5.Z i))]
+    | K.TRUE -> [Sm5.PUSH (Sm5.Val (Sm5.B true))]
+    | K.FALSE -> [Sm5.PUSH (Sm5.Val (Sm5.B false))]
+    | K.UNIT -> [Sm5.PUSH (Sm5.Val (Sm5.Unit))]
+    | K.VAR name -> [Sm5.PUSH (Sm5.Id name)]
     | K.ADD (e1, e2) -> trans e1 @ trans e2 @ [Sm5.ADD]
+    | K.SUB (e1, e2) -> trans e1 @ trans e2 @ [Sm5.SUB]
+    | K.MUL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.MUL]
+    | K.DIV (e1, e2) -> trans e1 @ trans e2 @ [Sm5.DIV]
+    | K.EQUAL (e1, e2) -> trans e1 @ trans e2 @ [Sm5.EQ]
+    | K.LESS (e1, e2) -> trans e1 @ trans e2 @ [Sm5.LESS]
+    | K.NOT exp -> trans exp @ [Sm5.NOT]
+
     | K.LETV (x, e1, e2) -> begin
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
