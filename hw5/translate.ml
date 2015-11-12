5d8d73a52e8055d65f6a889233acb19c2de123bb
Hyeon Kim <simnalamburt@gmail.com>
2015-11-12 15:30:28 +0900

    SM5: WRITE

--- hw5/translate.ml
+++ hw5/translate.ml
@@ -28,27 +28,31 @@ module Translator = struct
     | K.IF (econd, ethen, eelse) -> trans econd @ [Sm5.JTR (trans ethen, trans eelse)]
 
     | K.WHILE (econd, ebody)
-    -> failwith "Unimplemented"
+    -> failwith "WHILE Unimplemented"
     | K.FOR (econd, efrom, eto, ebody)
-    -> failwith "Unimplemented"
+    -> failwith "FOR Unimplemented"
 
     | K.LETV (x, e1, e2) -> begin
       trans e1 @ [Sm5.MALLOC; Sm5.BIND x; Sm5.PUSH (Sm5.Id x); Sm5.STORE] @
       trans e2 @ [Sm5.UNBIND; Sm5.POP]
     end
-
     | K.LETF (name, param, ebody, eafter) -> begin
       [Sm5.PUSH (Sm5.Fn (param, trans ebody)); Sm5.BIND name] @
       trans eafter @ [Sm5.UNBIND; Sm5.POP]
     end
 
     | K.CALLV (name, eparam)
-    -> failwith "Unimplemented"
+    -> failwith "CALLV unimplemented"
     | K.CALLR (name, param)
-    -> failwith "Unimplemented"
+    -> failwith "CALLR Unimplemented"
 
     | K.READ x -> [Sm5.GET; Sm5.PUSH (Sm5.Id x); Sm5.STORE; Sm5.PUSH (Sm5.Id x); Sm5.LOAD]
-
-    | K.WRITE exp
-    -> failwith "Unimplemented"
+    | K.WRITE exp -> begin
+      let tempname = "α" in
+      [Sm5.MALLOC; Sm5.BIND tempname] @ (* 변수 선언 *)
+      trans exp @ [Sm5.PUSH (Sm5.Id tempname); Sm5.STORE] @ (* 임시변수에 값 대입 *)
+      [Sm5.PUSH (Sm5.Id tempname); Sm5.LOAD; Sm5.PUT] @ (* 값 출력 *)
+      [Sm5.PUSH (Sm5.Id tempname); Sm5.LOAD] @ (* 스택 맨 위에 값 저장 *)
+      [Sm5.UNBIND; Sm5.POP] (* 임시변수 해제 *)
+    end
 end
