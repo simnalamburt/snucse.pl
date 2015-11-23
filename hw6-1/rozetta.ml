758309991dced6b21a64b017902a56a960ea7f61
Hyeon Kim <simnalamburt@gmail.com>
2015-11-23 23:54:54 +0900

    Simplify skeletal codes

--- hw6-1/rozetta.ml
+++ hw6-1/rozetta.ml
@@ -1,46 +1,56 @@
 (*
- * SNU 4190.310 Programming Languages 
+ * SNU 4190.310 Programming Languages
  * Homework "Rozetta" Skeleton
  * Jaeseung Choi (jschoi@ropas.snu.ac.kr)
  *)
 
-let trans_v : Sm5.value -> Sonata.value = function
-  | Sm5.Z z  -> Sonata.Z z
-  | Sm5.B b  -> Sonata.B b
+let trans_v (input: Sm5.value): Sonata.value =
+  match input with
+  | Sm5.Z z -> Sonata.Z z
+  | Sm5.B b -> Sonata.B b
   | Sm5.L _ -> raise (Sonata.Error "Invalid input program : pushing location")
   | Sm5.Unit -> Sonata.Unit
   | Sm5.R _ -> raise (Sonata.Error "Invalid input program : pushing record")
 
-(* TODO : complete this function *)
-let rec trans_obj : Sm5.obj -> Sonata.obj = function
+let trans_obj (input: Sm5.obj): Sonata.obj =
+  match input with
   | Sm5.Val v -> Sonata.Val (trans_v v)
   | Sm5.Id id -> Sonata.Id id
-  | Sm5.Fn (arg, command) -> failwith "TODO : fill in here"
+  | Sm5.Fn (arg, command) -> begin
+    (* TODO *)
+    failwith "TODO : fill in here"
+  end
 
-(* TODO : complete this function *)
-and trans' : Sm5.command -> Sonata.command = function
-  | Sm5.PUSH obj :: cmds -> Sonata.PUSH (trans_obj obj) :: (trans' cmds)
-  | Sm5.POP :: cmds -> Sonata.POP :: (trans' cmds)
-  | Sm5.STORE :: cmds -> Sonata.STORE :: (trans' cmds)
-  | Sm5.LOAD :: cmds -> Sonata.LOAD :: (trans' cmds)
-  | Sm5.JTR (c1, c2) :: cmds ->  failwith "TODO : fill in here"
-  | Sm5.MALLOC :: cmds -> Sonata.MALLOC :: (trans' cmds)
-  | Sm5.BOX z :: cmds -> Sonata.BOX z :: (trans' cmds)
-  | Sm5.UNBOX id :: cmds -> Sonata.UNBOX id :: (trans' cmds)
-  | Sm5.BIND id :: cmds -> Sonata.BIND id :: (trans' cmds)
-  | Sm5.UNBIND :: cmds -> Sonata.UNBIND :: (trans' cmds)
-  | Sm5.GET ::cmds -> Sonata.GET :: (trans' cmds)
-  | Sm5.PUT ::cmds -> Sonata.PUT :: (trans' cmds)
-  | Sm5.CALL :: cmds -> failwith "TODO : fill in here"
-  | Sm5.ADD :: cmds -> Sonata.ADD :: (trans' cmds)
-  | Sm5.SUB :: cmds -> Sonata.SUB :: (trans' cmds)
-  | Sm5.MUL :: cmds -> Sonata.MUL :: (trans' cmds)
-  | Sm5.DIV :: cmds -> Sonata.DIV :: (trans' cmds)
-  | Sm5.EQ :: cmds -> Sonata.EQ :: (trans' cmds)
-  | Sm5.LESS :: cmds -> Sonata.LESS :: (trans' cmds)
-  | Sm5.NOT :: cmds -> Sonata.NOT :: (trans' cmds)
+let rec trans_cmd (input: Sm5.cmd): Sonata.cmd =
+  match input with
+  | Sm5.PUSH obj -> Sonata.PUSH (trans_obj obj)
+  | Sm5.POP -> Sonata.POP
+  | Sm5.STORE -> Sonata.STORE
+  | Sm5.LOAD -> Sonata.LOAD
+  | Sm5.MALLOC -> Sonata.MALLOC
+  | Sm5.BOX z -> Sonata.BOX z
+  | Sm5.UNBOX id -> Sonata.UNBOX id
+  | Sm5.BIND id -> Sonata.BIND id
+  | Sm5.UNBIND -> Sonata.UNBIND
+  | Sm5.GET -> Sonata.GET
+  | Sm5.PUT -> Sonata.PUT
+  | Sm5.ADD -> Sonata.ADD
+  | Sm5.SUB -> Sonata.SUB
+  | Sm5.MUL -> Sonata.MUL
+  | Sm5.DIV -> Sonata.DIV
+  | Sm5.EQ -> Sonata.EQ
+  | Sm5.LESS -> Sonata.LESS
+  | Sm5.NOT -> Sonata.NOT
+  | Sm5.JTR (c1, c2) -> begin
+    (* TODO *)
+    failwith "TODO : fill in here"
+  end
+  | Sm5.CALL -> begin
+    (* TODO *)
+    failwith "TODO : fill in here"
+  end
+
+let rec trans (input: Sm5.command): Sonata.command =
+  match input with
   | [] -> []
-
-(* TODO : complete this function *)
-let trans : Sm5.command -> Sonata.command = fun command ->
-  failwith "TODO : fill in here"
+  | first::rest -> trans_cmd first :: trans rest
