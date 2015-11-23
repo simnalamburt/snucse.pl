92344cb6f62c6b730fbdbcdb1b0ffd3e370b2478
Hyeon Kim <simnalamburt@gmail.com>
2015-11-23 22:00:18 +0900

    숙제에서 안쓰는 파일 삭제

--- hw6-1/make.bat
+++
@@ -1,8 +0,0 @@
-ocamlc -c sm5.mli
-ocamlc -c sm5.ml
-ocamlc -c sonata.mli
-ocamlc -c sonata.ml
-ocamlc -c parser.ml
-ocamlc -c rozetta.ml
-ocamlc -c main.ml
-ocamlc -o run.exe sm5.cmo str.cma parser.cmo sonata.cmo rozetta.cmo main.cmo
