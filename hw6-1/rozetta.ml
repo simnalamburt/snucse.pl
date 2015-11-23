6e6c8f3ffeb7b98a845415e55de77aceb4c847d7
Hyeon Kim <simnalamburt@gmail.com>
2015-11-24 07:41:55 +0900

    JTR 구현
    1.  trans_cmd 함수 타입 변경
    2.  JTR 구현

--- hw6-1/rozetta.ml
+++ hw6-1/rozetta.ml
@@ -21,36 +21,32 @@ let trans_obj (input: Sm5.obj): Sonata.obj =
     failwith "TODO : fill in here"
   end
 
-let rec trans_cmd (input: Sm5.cmd): Sonata.cmd =
+let rec trans_cmd (input: Sm5.cmd): Sonata.command =
   match input with
-  | Sm5.PUSH obj -> Sonata.PUSH (trans_obj obj)
-  | Sm5.POP -> Sonata.POP
-  | Sm5.STORE -> Sonata.STORE
-  | Sm5.LOAD -> Sonata.LOAD
-  | Sm5.MALLOC -> Sonata.MALLOC
-  | Sm5.BOX z -> Sonata.BOX z
-  | Sm5.UNBOX id -> Sonata.UNBOX id
-  | Sm5.BIND id -> Sonata.BIND id
-  | Sm5.UNBIND -> Sonata.UNBIND
-  | Sm5.GET -> Sonata.GET
-  | Sm5.PUT -> Sonata.PUT
-  | Sm5.ADD -> Sonata.ADD
-  | Sm5.SUB -> Sonata.SUB
-  | Sm5.MUL -> Sonata.MUL
-  | Sm5.DIV -> Sonata.DIV
-  | Sm5.EQ -> Sonata.EQ
-  | Sm5.LESS -> Sonata.LESS
-  | Sm5.NOT -> Sonata.NOT
-  | Sm5.JTR (c1, c2) -> begin
-    (* TODO *)
-    failwith "TODO : fill in here"
-  end
+  | Sm5.PUSH obj  -> [Sonata.PUSH (trans_obj obj)]
+  | Sm5.POP       -> [Sonata.POP]
+  | Sm5.STORE     -> [Sonata.STORE]
+  | Sm5.LOAD      -> [Sonata.LOAD]
+  | Sm5.MALLOC    -> [Sonata.MALLOC]
+  | Sm5.BOX z     -> [Sonata.BOX z]
+  | Sm5.UNBOX id  -> [Sonata.UNBOX id]
+  | Sm5.BIND id   -> [Sonata.BIND id]
+  | Sm5.UNBIND    -> [Sonata.UNBIND]
+  | Sm5.GET       -> [Sonata.GET]
+  | Sm5.PUT       -> [Sonata.PUT]
+  | Sm5.ADD       -> [Sonata.ADD]
+  | Sm5.SUB       -> [Sonata.SUB]
+  | Sm5.MUL       -> [Sonata.MUL]
+  | Sm5.DIV       -> [Sonata.DIV]
+  | Sm5.EQ        -> [Sonata.EQ]
+  | Sm5.LESS      -> [Sonata.LESS]
+  | Sm5.NOT       -> [Sonata.NOT]
+  | Sm5.JTR (a,b) -> [Sonata.JTR (trans a, trans b)]
   | Sm5.CALL -> begin
     (* TODO *)
     failwith "TODO : fill in here"
   end
-
-let rec trans (input: Sm5.command): Sonata.command =
+and trans (input: Sm5.command): Sonata.command =
   match input with
   | [] -> []
-  | first::rest -> trans_cmd first :: trans rest
+  | first::rest -> trans_cmd first @ trans rest
