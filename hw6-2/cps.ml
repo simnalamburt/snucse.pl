85c09c1963058823975657eab8b0c92236ff2cb5
Hyeon Kim <simnalamburt@gmail.com>
2015-11-24 22:26:25 +0900

    Remove trailing whitespaces

--- hw6-2/cps.ml
+++ hw6-2/cps.ml
@@ -1,5 +1,5 @@
 (*
- * SNU 4190.310 Programming Languages 
+ * SNU 4190.310 Programming Languages
  * Continuation Passing Style Conversion Skeleton
  * Jaeseung Choi (jschoi@ropas.snu.ac.kr)
  *)
@@ -8,11 +8,11 @@ open M0
 
 let count = ref 0
 
-let new_name () = 
+let new_name () =
   let _ = count := !count + 1 in
   "x_" ^ (string_of_int !count)
 
-let rec alpha_conv exp subs = 
+let rec alpha_conv exp subs =
   match exp with
   | Num n -> Num n
   | Var x -> (try Var (List.assoc x subs) with Not_found -> Var x)
@@ -21,12 +21,12 @@ let rec alpha_conv exp subs =
     let subs' = (x, x') :: subs in
     Fn (x', alpha_conv e subs')
   | App (e1, e2) -> App (alpha_conv e1 subs, alpha_conv e2 subs)
-  | Rec (f, x, e) -> 
+  | Rec (f, x, e) ->
     let x' = new_name () in
     let f' = new_name () in
     let subs' = (f, f') :: (x, x') :: subs in
     Rec (f', x', alpha_conv e subs')
-  | Ifz (e1, e2, e3) -> 
+  | Ifz (e1, e2, e3) ->
     Ifz (alpha_conv e1 subs, alpha_conv e2 subs, alpha_conv e3 subs)
   | Add (e1, e2) -> Add (alpha_conv e1 subs, alpha_conv e2 subs)
   | Pair (e1, e2) -> Pair (alpha_conv e1 subs, alpha_conv e2 subs)
@@ -34,7 +34,7 @@ let rec alpha_conv exp subs =
   | Snd e -> Snd (alpha_conv e subs)
 
 (* TODO : Complete this function *)
-let rec cps' exp = 
+let rec cps' exp =
   let k = new_name () in
   match exp with
   (* Constant expressions *)
@@ -48,11 +48,11 @@ let rec cps' exp =
   | Add (e1, e2) ->
     let v1 = new_name () in
     let v2 = new_name () in
-    Fn (k, 
-        App (cps' e1, 
-            Fn (v1, 
-                App (cps' e2, 
-                    Fn (v2, 
+    Fn (k,
+        App (cps' e1,
+            Fn (v1,
+                App (cps' e2,
+                    Fn (v2,
                         App (Var k, Add (Var v1, Var v2))
                         )
                     )
