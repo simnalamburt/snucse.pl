25a27463efad7f12b7b1b644161f416656666040
Hyeon Kim <simnalamburt@gmail.com>
2015-12-03 19:05:27 +0900

    CPS 잡다한부분 구현

--- hw7/desugar.ml
+++ hw7/desugar.ml
@@ -11,11 +11,57 @@ let variable () =
   "α" ^ string_of_int !count
 
 let rec cps (input: xexp): xexp =
-  (* TODO *)
-  let k = variable () in
-  let h = variable () in
+  let k, h = variable (), variable () in
+  let biapp (efunc: xexp) (efirst: xexp) (esecond: xexp): xexp =
+    App (App (efunc, efirst), esecond)
+  in
   Fn (k, Fn (h, begin
-    input
+    match input with
+    | Num _ | Var _ -> App (Var k, input)
+    | Fn (param, ebody) -> App (Var k, Fn (param, cps ebody))
+    | App (efunc, eparam) -> begin
+      let f, v = variable (), variable () in
+      biapp (cps efunc) begin
+        Fn (f, begin
+          biapp (cps eparam) begin
+            Fn (v, begin
+              App (App (App (Var f, Var v), Var k), Var h)
+            end)
+          end (Var h)
+        end)
+      end (Var h)
+    end
+    | If (econd, ethen, eelse) -> begin
+      let foo (exp: xexp): xexp =
+        let tmp = variable () in
+        let body = Fn (tmp, App (Var k, Var tmp)) in
+        biapp (cps exp) body (Var h)
+      in
+      let vcond = variable () in
+      let body = Fn (vcond, If (Var vcond, foo ethen, foo eelse)) in
+      biapp (cps econd) body (Var h)
+    end
+    | Equal (eleft, eright) -> begin
+      (*
+      cps<left+right> = k -> { h -> {
+        // body
+        cps<left> (val1 -> {
+          cps<right> (val2 -> {
+            k (val1 + val2)
+          }) h
+        }) h
+      }};
+      *)
+
+      let val1, val2 = variable (), variable () in
+      biapp (cps eleft) (Fn (val1, begin
+        biapp (cps eright) (Fn (val2, begin
+          App (Var k, Equal (Var val1, Var val2))
+        end)) (Var h)
+      end)) (Var h)
+    end
+    (* TODO *)
+    | _ -> input
   end))
 
 let removeExn (input: xexp): xexp =
